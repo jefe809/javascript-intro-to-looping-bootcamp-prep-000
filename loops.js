7c6aeccc9eaa93c293734b1f641d865e87bbd006
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-06 20:58:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,5 +20,12 @@ function whileLoop(n){
 }
 
 function doWhileLoop(array){
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (array.length > 0 && maybeTrue());
+return array;
 }
