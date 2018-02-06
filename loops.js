a7c8712cb20d987c34c06add2f5ab15dba753563
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-06 20:09:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array){
   for (i= 0; i < 25; i++ ){
   if (i === 1){
     console.log("I am 1 strange loop.")
-  }else if (i > 1)
+  }else if (i >= 0)
   {
     console.log(`"I am ${i} strange loops."`)
   }
